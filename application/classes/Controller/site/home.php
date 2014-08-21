cd8d2bede9c11272795eee9ebcfdd5e322663aac
Mahieddine Yaker <myaker@runiso.com>
2014-08-21 17:12:34 +0200

    nouveau

--- application/classes/Controller/site/home.php
+++ application/classes/Controller/site/home.php
@@ -6,14 +6,19 @@
 class Controller_Home extends Controller {
 
     protected $html_header = NULL;
+    protected 
 
+    public function __construct()
+    {
+        parent::__construct();
 
+    }
     public function before()
     {
         $this->html_header = new HTML_Header();
         $this->html_header->set_title('Partage de mot de passe');
         $this->html_header->add(new HTML_Header_Meta(array('charset' => 'utf-8')));
-
+        
 
     }
 
